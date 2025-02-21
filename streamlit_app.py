f10f89c02bbcbc4091702429add75eefc89058d5
carriecheng0924 <74885276+carriecheng0924@users.noreply.github.com>
2025-02-21 15:40:53 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ for message in st.session_state.messages:
 
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("You are a therapist that decides which therapy strategy to treat the patient."):
 
     # Store and display the current prompt.
     st.session_state.messages.append({"role": "user", "content": prompt})
