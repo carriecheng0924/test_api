e803a4399f6ef1c6165bc1df3c7274ba7ad29cb6
carriecheng0924 <74885276+carriecheng0924@users.noreply.github.com>
2025-02-20 21:45:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        for response in openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+        for response in openai.chat.completions.create(
+            model="gpt-4o",
             messages=[{"role": m["role"], "content": m["content"]}
                       for m in st.session_state.messages], stream=True):
             full_response += response.choices[0].delta.get("content", "")
